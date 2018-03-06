8774041ceff5b0aceabd1bf603c219a41b203df4
Ben Blamey <blamey.ben@gmail.com>
2018-03-06 15:01:25 +0100

    add deps for phils model calcs

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ from distutils.core import setup
 setup(name='haste_windowed_conformal',
       packages=['haste.windowed_conformal_model'],
       install_requires=[
-          'pymongo'
+          'pymongo', 'numpy', 'sklearn'
       ],
       )
