ba4ee59f317fd83a9d5b1f819cfdb0ad51e70bab
Philip Harrison <philip.harrison@farmbio.uu.se>
2018-03-07 14:12:36 +0100

    temporary file with in prep function tsfeatures (computing time-series features) and phils TODO list

---
+++ haste/windowed_conformal_model/temp_phil.py
@@ -0,0 +1,87 @@
+# tsfeatures: computes features from time-series data
+# ---------------------------------------------------
+
+# pygam was the closest thing I could find in python to R's mgcv (a great package for GAMs)
+#       pip install pygam
+# pygam says to do the following installation to install scikit-sparse and nose
+# I have nose but installed scikit-sparse on my mac with:
+#       conda install -c conda-forge scikit-sparse
+# the features I compute below are the same as those I used in my R demo
+# later I will explore other potential features to extract
+# such as those possible with the python package tsfresh (as used by Fredrik in his work)
+
+import numpy as np
+import pandas as pd
+from pygam import LinearGAM
+
+def tsfeatures(time_feature, time_steps, end_time):
+    """
+    :param time_feature: 1d ndarray of features (e.g. GFP sum at each of time_steps).
+    :param time_steps: 1d ndarray e.g. 0:7, 0:15, but can deal with missing data.
+    :param end_time: either 8, 16, 24, ..., 88.
+    :return: 4 time-series features (mean; standard deviation; mean(trend); mean(trend change))
+    """
+    feat = np.zeros(4)
+    
+    X = pd.DataFrame(time_steps)
+    y = pd.Series(time_feature)
+    
+    gam = LinearGAM(n_splines=min(25,len(y))).gridsearch(X, y)
+    
+    feat[0] = np.mean(y) # mean of raw data
+    feat[1] = np.std(y)  # standard deviation of raw data
+    
+    endt = end_time
+    y_pred = gam.predict(pd.DataFrame(np.arange(endt)))
+    
+    D1 = np.zeros(endt)
+    D2 = np.zeros(endt)
+    
+    for i in range(1, endt-1):
+        D1[i] = 0.5*(y_pred[i+1] - y_pred[i-1])  # 1st derivative from smooth
+        D2[i] = y_pred[i+1] - 2*y_pred[i] + y_pred[i-1]  # 2nd derivative 
+    
+    feat[2] = np.mean(D1[1:(endt-1)])
+    feat[3] = np.mean(D2[1:(endt-1)])
+    
+    return feat
+
+if __name__ == '__main__':
+    # strangely when running this "poor man's unit test" from the terminal I get an ImportWarning
+    # but not when I run it in a jupyter notebook!?
+    # in both cases things appear to work though...
+    tife = np.sort(np.random.random(8))
+    tist = np.arange(8)
+    enti = 8
+    tsfe = tsfeatures(time_feature=tife, time_steps=tist, end_time=enti)
+    # if just using GFP will run the above function twice appending the results as we go
+    # so run for GFP sum and get 4 features
+    # then run with GFP correlation and append four more features to the 1d ndarray
+    # if we use the LNP data then similarly just append four new features 
+    # for each granulometry disc size we decide to use
+    # in my R demo I used disk sizes 2:4
+    # so for the "full" model I had 32 features to fit the models to
+    print(tsfe)
+
+
+# Phil's TODO list (with help as needed from Ben & Håkan)
+# ------------------------------------------------------
+
+# necessary for streaming demo to work
+# ------------------------------------
+# create 1d ndarray with response variables for "training" data (sum(GFP correlation) through time >= 20)
+# exclude B10 from all analyses (i.e. not part of "training" or "test" data) [had a fibre across image field]
+# use tsfeatures above on "training" data to make a 3d ndarray (train_data)
+# dimensions = number of training wells X number of features X number of time windows
+# number of features = 8 if just using GFP sum and GFP correlation
+# number of time windows = 11 [= 8/88]: first = 0:7, second = 0:15, etc
+# standardise train_data and store the mean and sd of the standardisations for use on the "test" data
+# standardisation happens after using "tsfeatures" function but before running "interestingness" TCP function
+
+# for my own explorations and additional bits and pieces for AZ demo
+# ------------------------------------------------------------------
+# test on my mac (non-streaming)
+# are results comparable to those gotten with R?
+# plot results also with python (as from end of R demo)
+# tables of results from python run (again as from end of my R demo)
+
