a25dc1ef828e77fce5b0ddf9fa9bd6b5d8552b43
Ben Blamey <blamey.ben@gmail.com>
2018-03-09 09:42:17 +0100

    add comment

--- haste/windowed_conformal_model/time_series_features.py
+++ haste/windowed_conformal_model/time_series_features.py
@@ -14,6 +14,8 @@ import numpy as np
 import pandas as pd
 from pygam import LinearGAM
 
+# TODO: BB is confused about the windowing - do we call this the function the whole web-substream time-series,
+# or just the most recent timewindow? If the latter, don’t we need a `start_time` also?
 
 def time_series_features(feature_time_series, timestamps, end_time):
     """
