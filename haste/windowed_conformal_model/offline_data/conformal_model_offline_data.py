b2e25bba19d8cbec6375acf6df228689861c5dc4
Ben Blamey <blamey.ben@gmail.com>
2018-03-19 15:04:31 +0100

    remove debug prints, add todos

--- haste/windowed_conformal_model/offline_data/conformal_model_offline_data.py
+++ haste/windowed_conformal_model/offline_data/conformal_model_offline_data.py
@@ -3,15 +3,15 @@ import pkg_resources
 
 
 def _load_offline_resource(resource_path):
-    print(__file__, flush=True)
     # Load the file correctly if we've been imported (and even if we are an egg!)
     file_path = pkg_resources.resource_filename(__name__, resource_path)
-    print(file_path, flush=True)
     return np.load(file_path)
 
 
 TRAIN_FEATURES_STANDARDIZED = _load_offline_resource('train_features_standardized.npy')
 STANDARDIZING_VALUES = _load_offline_resource('standardizing_values.npy')
 
+# Consider writing these as literals for clarity:
+# might be nicer if these were dictionaries with wells as keys, to avoid potential for confusion
 TRAIN_Y = _load_offline_resource('train_y.npy')
 TEST_Y = _load_offline_resource('test_y.npy')
